9d201ca8635ba8c085383fbe2aac2d39387c8f34
Tomasz Jurek <jur.tomasz13@gmail.com>
2022-08-12 13:23:17 +0200

    modifications

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const queryClient = new QueryClient();
