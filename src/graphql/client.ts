19a74d3b853d37efbe0433600003614349913108
Tomasz Jurek <jur.tomasz13@gmail.com>
2024-03-18 19:18:20 +0100

    Updates

--- src/graphql/client.ts
+++
@@ -1,6 +0,0 @@
-import { GraphQLClient } from "graphql-request";
-
-const GRAPHQL_URI =
-  "https://swapi-graphql.netlify.app/.netlify/functions/index";
-
-export const client = new GraphQLClient(GRAPHQL_URI);
